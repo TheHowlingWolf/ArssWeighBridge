e69c775a782f9fc1d0e63ecade2aaf265f025c0a
Debraj Sengupta <43552823+TheHowlingWolf@users.noreply.github.com>
2020-01-13 20:36:54 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,64 +1,75 @@
+
+
+
 //signup
 const site = document.getElementById('siteSignUp');
-site.addEventListener('submit', (e)=>{
+site.addEventListener('submit', (e) => {
     //preventing default refresh
     e.preventDefault();
 
     //get user info
     const siteName = site['sitename'].value;
+    const email = site['email'].value;
     const password = site['password'].value;
     const devId = site['devId'].value;
+console.log(email);
     //console.log(siteName+'\n'+devId+'\n'+password);
 
     //signup the user using firebase
-     auth.createUserWithEmailAndPassword(siteName,password)
-     .then(cred => {
-         document.querySelector('.Register').classList.add('d-none');
-         document.querySelector('.con-reg').classList.remove('d-none'); 
-         setTimeout((time)=>{ 
-            site.reset();
-            auth.signOut().then(()=>{
-            document.querySelector('.login').classList.remove('d-none');
-            document.querySelector('.intro').classList.add('d-none');
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+            //  console.log(cred.user.uid);
             document.querySelector('.Register').classList.add('d-none');
-            document.querySelector('.con-reg').classList.add('d-none');
-            document.querySelector('.register-nav').style.borderBottom = '0px solid #ffc107';
-            document.querySelector('.home-nav').style.borderBottom = '0px solid #ffc107';
-            document.querySelector('.login-nav').style.borderBottom = '2px solid #ffc107';
-            })
-         },2000);
-     }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+            document.querySelector('.con-reg').classList.remove('d-none');
+            setTimeout((time) => {
+                site.reset();
+                auth.signOut().then(() => {
+                    document.querySelector('.login').classList.remove('d-none');
+                    document.querySelector('.intro').classList.add('d-none');
+                    document.querySelector('.Register').classList.add('d-none');
+                    document.querySelector('.con-reg').classList.add('d-none');
+                    document.querySelector('.register-nav').style.borderBottom = '0px solid #ffc107';
+                    document.querySelector('.home-nav').style.borderBottom = '0px solid #ffc107';
+                    document.querySelector('.login-nav').style.borderBottom = '2px solid #ffc107';
+                    db.collection.('admin').collection('UserProfile').add({
+                        uid: cred.user.uid,
+                        siteName: siteName,
+                        deviceId: devId
+                    });
+                })
+            }, 2000);
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
 
-        document.querySelector('.error').innerHTML = `OPPS! ${errorMessage}`;
+            document.querySelector('.error').innerHTML = `OPPS! ${errorMessage}`;
 
-    });
-}); 
+        });
+});
 
 //login users
 const Login = document.querySelector('#login-form');
-Login.addEventListener('submit',(e)=>{
+Login.addEventListener('submit', (e) => {
     e.preventDefault();
-     
+
     //get user info
     const lg_email = Login['login_email'].value;
     const lg_pass = Login['login_password'].value;
 
-     auth.signInWithEmailAndPassword(lg_email,lg_pass)
-    .then((cred)=>{
-        document.querySelector('.login').classList.add('d-none');
-        document.querySelector('.con-log').classList.remove('d-none');
-        document.querySelector('.lgerror').innerHTML = '';
-        Login.reset();
-        setTimeout((time)=>{ 
-            window.location.assign("user.html");
-         },2000);
-    })
-    .catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+    auth.signInWithEmailAndPassword(lg_email, lg_pass)
+        .then((cred) => {
+            document.querySelector('.login').classList.add('d-none');
+            document.querySelector('.con-log').classList.remove('d-none');
+            document.querySelector('.lgerror').innerHTML = '';
+            Login.reset();
+            setTimeout((time) => {
+                window.location.assign("user.html");
+            }, 2000);
+        })
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
 
-        document.querySelector('.lgerror').innerHTML = `OPPS! ${errorMessage}`;
-    });
+            document.querySelector('.lgerror').innerHTML = `OPPS! ${errorMessage}`;
+        });
 });
