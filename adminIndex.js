eb026802e378f745f2c4934adbcc56962cd4b77d
Debraj Sengupta <theuniqueraj@gmail.com>
2020-01-15 12:08:47 +0530

    2
    2

--- adminIndex.js
+++ adminIndex.js
@@ -35,7 +35,16 @@ function back() {
   document.querySelector(".scrollable").classList.remove("d-none");
   document.querySelector(".details-site").classList.add("d-none");
   document.querySelector("#download-slips").classList.add("d-none");
+  document.querySelector(".settings-menu").classList.add("d-none");
   document.getElementById("site-details").innerHTML = "";
+
+}
+
+function settings(){
+  document.querySelector(".scrollable").classList.add("d-none");
+  document.querySelector(".details-site").classList.add("d-none");
+  document.querySelector("#download-slips").classList.add("d-none");
+  document.querySelector(".settings-menu").classList.remove("d-none");
 }
 
 var sites = document.getElementById("sites");
@@ -53,8 +62,8 @@ db.collection("UserProfile")
                 ${doc.data().siteName}
                 </h4>
                 <button class="btn  border-danger bg-dark text-white mt-1 " onclick="moreDetails('${
-          doc.data().uid
-          }')">View
+                  doc.data().uid
+                }')">View
                     Details</button>
             </div>`;
     });
@@ -67,7 +76,7 @@ filter.addEventListener("submit", e => {
   const startDate = new Date(filter["startDate"].value).getTime();
   const endDate = new Date(filter["endDate"].value).getTime();
 
-
+  
   var fileTitle;
   db.collection("UserProfile")
     .where("uid", "==", siteChose)
@@ -116,6 +125,7 @@ function downloadPage() {
   document.querySelector(".scrollable").classList.add("d-none");
   document.querySelector(".details-site").classList.add("d-none");
   document.querySelector("#download-slips").classList.remove("d-none");
+  document.querySelector(".settings-menu").classList.add("d-none");
 }
 
 function convertToCSV(objArray) {
@@ -188,45 +198,3 @@ function adminback() {
   document.querySelector(".details-site").classList.remove("d-none");
   document.querySelector("#download-slips").classList.add("d-none");
 }
-
-
-const searchBar = document.getElementById('search-ticket');
-searchBar.addEventListener('submit', (e) => {
-  e.preventDefault();
-  var searchItem = searchBar['searchTicket'].value;
-  console.log(searchItem)
-  document.getElementById('site-details').innerHTML = "";
-  db.collection("users")
-    .doc(siteChose)
-    .collection("Ticket")
-    .where("Ticket_No", "==", Number(searchItem))
-    .get()
-    .then(snapshot => {
-      dataLength = snapshot.docs.length;
-      datas = snapshot.docs;
-      console.log(datas);
-      console.log(datas[0].data());
-
-      if (dataLength > 0) {
-        document.getElementById('site-details').innerHTML = `
-          <tr>
-          <td >${datas[0].data().Ticket_No}</td>
-          <td >${datas[0].data().Customer_Name}</td>
-          <td > ${datas[0].data().Vehicle_Number}</td> 
-          <td > ${datas[0].data().Material}</td>
-          <td > ${datas[0].data().Supplier}</td>
-          <td > ${datas[0].data().Tire_Weight}</td>
-          <td>${datas[0].data().Loaded_Weight} </td>
-          <td>${datas[0].data().Gross_Weight}</td>
-          <td>${new Date(datas[0].data().Gross_Weight_Time).toLocaleTimeString()}</td>
-          <td>${new Date(datas[0].data().Gross_Weight_Time).toLocaleDateString()}</td>
-          
-          </tr>
-        `
-      }
-      else{
-        document.getElementById('site-details').innerHTML = 'No Results Found';
-      }
-    })
-
-})
